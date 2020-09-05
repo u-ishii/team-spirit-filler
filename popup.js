5c96c0f8306bf2335ed6d6b6a44f1b844234fcf5
u-ishii <yuki.ishii.business@gmail.com>
2020-09-05 19:24:27 +0900

    simplify

--- popup.js
+++ popup.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const remarkContent = document.getElementById('remarkContent');
-  const remarkExcludedDays = document.getElementById('remarkExcludedDays');
-  const fillRemarksSubmit = document.getElementById('fillRemarksSubmit');
-  fillRemarksSubmit.addEventListener('click', () => {
-    console.log('xxxx');
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        {
-          remarkContentText: remarkContent.value,
-          remarkExcludedDaysText: remarkExcludedDays.value
-        }
-      );
-    });
+const remarkContent = document.getElementById('remarkContent');
+const remarkExcludedDays = document.getElementById('remarkExcludedDays');
+const fillRemarksSubmit = document.getElementById('fillRemarksSubmit');
+fillRemarksSubmit.addEventListener('click', () => {
+  console.log('xxxx');
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        remarkContentText: remarkContent.value,
+        remarkExcludedDaysText: remarkExcludedDays.value
+      }
+    );
   });
 });
