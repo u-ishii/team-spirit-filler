79a0db5a488e829be1e0a13de88fb94a60feaec2
u-ishii <yuki.ishii.business@gmail.com>
2020-09-06 02:48:33 +0900

    make extension dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,10 @@
 module.exports = {
+  entry: {
+    popup: './src/popup.js',
+    content_script: './src/content_script.js'
+  },
+  output: {
+    path: __dirname,
+    filename: 'extension/dist/[name].js'
+  }
 };
